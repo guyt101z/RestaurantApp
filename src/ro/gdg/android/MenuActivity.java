ea321d70f288a38088acd45b34baab642f54d057
dianaghele <diana.ghele@threepillarglobal.com>
2012-06-12 15:51:18 +0300

    renamed some resources, added some styles

--- src/ro/gdg/android/MenuActivity.java
+++ src/ro/gdg/android/MenuActivity.java
@@ -4,39 +4,45 @@ import ro.gdg.android.util.SelectionListener;
 import android.app.ExpandableListActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Filter.FilterListener;
 
-public class MenuActivity extends ExpandableListActivity implements TextWatcher, SelectionListener, FilterListener{
+public class MenuActivity extends ExpandableListActivity implements
+		TextWatcher, SelectionListener, FilterListener {
 
 	@Override
 	public void onFilterComplete(int arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void afterTextChanged(Editable s) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void beforeTextChanged(CharSequence s, int start, int count,
 			int after) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onTextChanged(CharSequence s, int start, int before, int count) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onChangeSelection(int count) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void finish(View view) {
+		// TODO
 	}
 
 }
